902da60c3184048990d9ca43445622353482e67f
DimaSorokin <demonsorokin29@gmail.com>
2019-11-03 22:17:00 +0200

    a thread can have replies

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Reply;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ReplyTest extends TestCase
+{
+   function test_it_has_an_owner(){
+        $reply = factory(Reply::class)->create();
+        $this->assertInstanceOf(User::class, $reply->owner);
+   }
+}
